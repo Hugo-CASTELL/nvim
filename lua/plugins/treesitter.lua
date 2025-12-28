03cbaf2f20541fb2b7ecf966b398d5e30468251f
Hugo-CASTELL <hugo.castell@outlook.fr>
2025-12-28 20:06:56 +0100

    Updated treesitter config from official doc

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,10 +1,10 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
-  config = function () 
-    local configs = require("nvim-treesitter.configs")
+  config = function ()
+    local treesitter = require("nvim-treesitter")
 
-    configs.setup({
+    treesitter.setup({
       ensure_installed = { "java", "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "typescript", "html" },
       sync_install = false,
       highlight = { enable = true },
